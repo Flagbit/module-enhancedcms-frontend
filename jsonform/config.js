c928695dbd33a748fd92108d2a1e3976efe6fdb9
Alexander Nickel <mr.alexander.nickel@gmail.com>
2017-02-24 11:01:03 +0100

    Add youtubeId validation

--- jsonform/config.js
+++ jsonform/config.js
@@ -1,4 +1,4 @@
-define(['jquery', './plugins/productSelect.min.js', './plugins/imageSelect.min.js', './plugins/sideSizes.min.js', './plugins/size.min.js', './plugins/color.min.js', './plugins/opacity.min.js', './plugins/replaceQuotes.min.js', './jsonform.min.js'], function (_jquery, _productSelectMin, _imageSelectMin, _sideSizesMin, _sizeMin, _colorMin, _opacityMin, _replaceQuotesMin) {
+define(['jquery', './plugins/productSelect.min.js', './plugins/imageSelect.min.js', './plugins/sideSizes.min.js', './plugins/size.min.js', './plugins/youtubeId.min.js', './plugins/color.min.js', './plugins/opacity.min.js', './plugins/replaceQuotes.min.js', './jsonform.min.js'], function (_jquery, _productSelectMin, _imageSelectMin, _sideSizesMin, _sizeMin, _youtubeIdMin, _colorMin, _opacityMin, _replaceQuotesMin) {
   'use strict';
 
   var _jquery2 = _interopRequireDefault(_jquery);
@@ -10,6 +10,8 @@ define(['jquery', './plugins/productSelect.min.js', './plugins/imageSelect.min.j
   var _sideSizesMin2 = _interopRequireDefault(_sideSizesMin);
 
   var _sizeMin2 = _interopRequireDefault(_sizeMin);
+
+  var _youtubeIdMin2 = _interopRequireDefault(_youtubeIdMin);
 
   var _colorMin2 = _interopRequireDefault(_colorMin);
 
@@ -27,6 +29,7 @@ define(['jquery', './plugins/productSelect.min.js', './plugins/imageSelect.min.j
   JSONForm.fieldTypes['imageSelect'] = _imageSelectMin2.default;
   JSONForm.fieldTypes['sideSizes'] = _sideSizesMin2.default;
   JSONForm.fieldTypes['size'] = _sizeMin2.default;
+  JSONForm.fieldTypes['youtubeId'] = _youtubeIdMin2.default;
   JSONForm.fieldTypes['color'] = _colorMin2.default;
   JSONForm.fieldTypes['opacity'] = _opacityMin2.default;
   JSONForm.fieldTypes['replaceQuotes'] = _replaceQuotesMin2.default;
---
+++ jsonform/plugins/youtubeId.js
@@ -0,0 +1,12 @@
+define(["exports"], function (exports) {
+    "use strict";
+
+    Object.defineProperty(exports, "__esModule", {
+        value: true
+    });
+    exports.default = {
+        fieldtemplate: true,
+        inputfield: true,
+        template: "\n<div class=\"ecms-config-compile\">\n    <div ecms-config-size-unit>    \n        <md-input-container>\n            <label for=\"<%= id %>\"><%= node.title %></label>\n            <input type=\"text\" ng-pattern=\"youtubeId\" ng-model=\"currentData.<%= node.name %>\" />\n        </md-input-container>\n    </div>\n</div>\n"
+    };
+});
