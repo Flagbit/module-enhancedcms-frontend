1394b34488add912c44739b7b1f62fe5ee2552e7
Alexander Nickel <mr.alexander.nickel@gmail.com>
2017-02-24 11:22:25 +0100

    Add YoutubeIdDirective

--- jsonform/plugins/youtubeId.js
+++ jsonform/plugins/youtubeId.js
@@ -7,6 +7,6 @@ define(["exports"], function (exports) {
     exports.default = {
         fieldtemplate: true,
         inputfield: true,
-        template: "\n<div class=\"ecms-config-compile\">\n    <div ecms-config-size-unit>    \n        <md-input-container>\n            <label for=\"<%= id %>\"><%= node.title %></label>\n            <input type=\"text\" ng-pattern=\"youtubeId\" ng-model=\"currentData.<%= node.name %>\" />\n        </md-input-container>\n    </div>\n</div>\n"
+        template: "\n<div class=\"ecms-config-compile\">\n    <div ecms-config-youtube-id>    \n        <md-input-container>\n            <label for=\"<%= id %>\"><%= node.title %></label>\n            <input type=\"text\" ng-pattern=\"youtubeId\" ng-model=\"currentData.<%= node.name %>\" />\n        </md-input-container>\n    </div>\n</div>\n"
     };
 });
