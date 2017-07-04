65ae5ae64bb4a2fbaa98d401b4f3f29e06e477b2
Alexander Nickel <mr.alexander.nickel@gmail.com>
2017-07-04 14:14:26 +0200

    Fix carousel-selector

--- 0.0.2/modules/CarouselBootstrap.js
+++ 0.0.2/modules/CarouselBootstrap.js
@@ -3,7 +3,7 @@ define(["require", "exports"], function (require, exports) {
     Object.defineProperty(exports, "__esModule", { value: true });
     var CarouselBootstrap = (function () {
         function CarouselBootstrap() {
-            var carousels = document.getElementsByClassName('carousel');
+            var carousels = document.querySelectorAll('.ecms-widget-carousel .carousel');
             console.log('CAROUSEL BOOtSTRAP', carousels);
             if (carousels) {
                 for (var i = 0; i < carousels.length; i++) {
